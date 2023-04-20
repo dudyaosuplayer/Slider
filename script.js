3b48c0caa732d416b6adb4709f74270e13a494ca
dudyaosuplayer <sasch2010pavlov@mail.ru>
2023-04-20 19:20:23 +0400

    'commit'

---
+++ script.js
@@ -0,0 +1,19 @@
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.slider-rigth').addEventListener('click', () => {
+    offset -= 256;
+    if (offset < -768) {
+        offset = 0;
+    }
+    sliderLine.style.left = offset + 'px';
+    
+})
+
+document.querySelector('.slider-left').addEventListener('click', () => {
+    offset += 256;
+    if (offset > 0) {
+        offset = -768;
+    }
+    sliderLine.style.left = offset + 'px';  
+})
